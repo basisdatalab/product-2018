e8f27fa8a9586e2d02b94a8d75b2e7b3fc017084
Helmi Satria Nugraha <satriahelmi@gmail.com>
2018-09-27 22:09:36 +0700

    pertemuan 27 Sept, database, mongoose, body-parser, hbs (loop, variabel)

--- app.js
+++ app.js
@@ -1,78 +1,83 @@
 const express = require('express')
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: false }))
+
 mongoose.connect('mongodb://helmisatria:password123@ds036967.mlab.com:36967/product-trial', { useNewUrlParser: true }, (error) => {
   if (error) {
-    console.log('Ada error woy!');
+    console.log('Ada error euy')
     return
   }
-  console.log('Berhasil connect ke db! Horee');
-});
 
-const Cat = mongoose.model('Cat', { name: String });
+  console.log('Berhasil connect ke DB')
+})
+
+const Mahasiswa = mongoose.model('Mahasiswa', {
+  nama: String,
+  nim: String
+})
 
 // ini buat render file ejs
-app.set('view engine', 'ejs');
+app.set('view engine', 'hbs');
 
 // ini buat change default folder views
 app.set("views", path.join(__dirname, "halamans"));
 
-// app.get
-app.get('/cihuy', (req, res) => {
-  const variabelKirim = "Bams 123"
-
-  console.log(variabelKirim);
+app.get('/', (req, res) => {
   
-
-  Cat.create({ name: 'KOCHEEENG' }, (err, result) => {
-    if (err) {
-      console.log('====================================');
-      console.log(err);
-      console.log('====================================');
-    }
-
-    console.log(result);
-    
+  let variabel = {
+    us: 'Ini dari variabel'
   }
-  )
 
-  
-  res.render('cihuy.ejs', {
-    iniVarDiFrontend: variabelKirim,
-    cobalagi: 'Holaaa!',
-    perlihatkan: false,
-    list: [1, 3, 5, 7, 9]
+  // 
+  res.render('index', variabel)
+})
+
+app.get('/mahasiswa', (req, res) => {
+  Mahasiswa
+  .find() // SELECT * FROM MAHASISWA
+  .then((allMahasiswa) => { // KALAU GA ERROR, allMahasiswa itu hasil SELECT nya
+    res.send(allMahasiswa)
+  })
+  .catch(error => { // KALAU ERROR, variabel error akan ada valuenya
+    res.send(error)
   })
 })
 
-
-
-app.get('/json', (request, response) => {
-  // res.send('Halo basdat!')
-  const objek = {
-    nama: 'Hevyka',
-    topik: 'Product'
-  }
-  
-  response.send("objek")
+app.get('/mahasiswa/baru', (req, res) => {
+  // Get semua mahasiswa
+  // Tampilin di mahasiswaBaru.ejs
+  Mahasiswa
+    .find()
+    .then(allMahasiswa => {
+      console.log(allMahasiswa)
+      res.render('mahasiswaBaru', {
+        mhs: allMahasiswa
+      })
+    })
+    .catch((error) => {
+      res.send(error)
+    })
 })
 
-app.get('/hevyka', (req, res) => {
-  res.send('Halo Hevyka')
-})
+// Create Mahasiswa
+app.post('/mhs', (req, res) => {
+  console.log(req.body)
 
-app.get('/products', (req, res) => {
-  res.send('Hoam')
-})
-
-app.get('/products/:cobaja', (req, res) => {
-  const apaya = req.params.cobaja
-  
-  res.send('Ini halaman product: ' + apaya)
-  // perintah, query database, proses
+  Mahasiswa.create({
+    nama: req.body.namaMhs
+  })
+  .then((hasil) => {
+    console.log(hasil)
+    res.redirect('/mahasiswa')
+  })
+  .catch((error) => {
+    console.log(error);
+  })
 })
 
 app.get('*', (req, res) => {
@@ -82,11 +87,3 @@ app.get('*', (req, res) => {
 app.listen(8000, () => {
   console.log('8000')
 })
-
-function namaFunction() {
-
-}
-
-const namaFunction2 = () => {
-  
-}
